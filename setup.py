dbe46bc09c1627f9bdd1b17337d84cd731f29812
mknorps <maria.knorps@gmail.com>
2019-06-30 22:24:00 +0200

    merge

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis and modelling of Polish birds songs',
+    author='WiMLDS',
+    license='MIT',
+)
